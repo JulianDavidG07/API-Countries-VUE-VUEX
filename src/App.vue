a979f490e60397c7c9512dcdf378139d61577c64
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-07-24 23:39:01 -0500

    [ADD] styles to header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <div class="flex justify-center bg-blue-700">
-      <router-link class="px-2" to="/">Home</router-link>
-      <router-link class="px-2" to="/about">About</router-link>
+      <router-link to="/" class="px-2 hover:bg-yellow-300" exact>Home</router-link>
+
+      <router-link to="/about" class="px-2 hover:bg-yellow-300" exact>About</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<style>
+.router-link-active {
+  background: rgb(241, 241, 124);
+}
+</style>
 
